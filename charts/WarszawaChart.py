7ab0268798f3418180e90e5b539ce34829b2e3a1
anison99_ <anita.zych9941@gmail.com>
2023-05-18 15:22:30 +0200

    creating charts for all cities in projects

---
+++ charts/WarszawaChart.py
@@ -0,0 +1,36 @@
+import sqlite3
+import matplotlib.pyplot as plt
+
+# Łączenie z bazą danych
+conn = sqlite3.connect('dane_pogodowe.db')
+c = conn.cursor()
+
+# Pobieranie danych z bazy danych
+c.execute("SELECT data, temperatura, wilgotnosc FROM pogoda_warszawa")
+
+dates = []
+temperatures = []
+humidities = []
+
+# Przetwarzanie wyników zapytania
+for row in c.fetchall():
+    dates.append(row[0])
+    temperatures.append(row[1])
+    humidities.append(row[2])
+
+# Zamykanie połączenia z bazą danych
+conn.close()
+
+# Tworzenie wykresu
+plt.figure(figsize=(10, 6))
+plt.plot(dates, temperatures, label='Temperatura (°C)')
+plt.plot(dates, humidities, label='Wilgotność (%)')
+plt.xlabel('Data')
+plt.ylabel('Wartość')
+plt.title('Wykres pogodowy na najbliższe 5 dni - Warszawa')
+plt.xticks(rotation=45)
+plt.legend()
+
+# Wyświetlanie wykresu
+plt.tight_layout()
+plt.show()
